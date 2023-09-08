65589f67b33b43dd13d00a3313395f45129954e3
Steven Hedges <steven.hedges34@gmail.com>
2023-09-08 15:19:39 -0400

    sprite moves around screen

---
+++ src/main.rs
@@ -0,0 +1,73 @@
+use bevy::{prelude::*, render::camera::ScalingMode};
+
+#[derive(Component)]
+pub struct Player {
+    pub speed: f32,
+}
+
+fn main() {
+    App::new()
+        .add_plugins(
+            DefaultPlugins
+                .set(ImagePlugin::default_nearest())
+                .set(WindowPlugin {
+                    primary_window: Some(Window {
+                        title: "2d Game".into(),
+                        resolution: (640.0, 480.0).into(),
+                        resizable: false,
+                        ..default()
+                    }),
+                    ..default()
+                })
+                .build(),
+        )
+        .add_systems(Startup, setup)
+        .add_systems(Update, character_movment)
+        .run();
+    
+}
+
+fn setup(mut commands: Commands, asset_server: Res<AssetServer>) {
+    let mut camera = Camera2dBundle::default();
+
+    camera.projection.scaling_mode = ScalingMode::AutoMin {
+        min_width: 600.0,
+        min_height: 800.0,
+    };
+
+    commands.spawn(camera);
+
+    let texture = asset_server.load("test-character.png");
+
+    commands.spawn((
+        SpriteBundle {
+            texture,
+            ..default()
+        },
+        Player { speed: 100.0 }
+    ));
+}
+
+fn character_movment(
+    mut characters: Query<(&mut Transform, &Player)>,
+    input: Res<Input<KeyCode>>,
+    time: Res<Time>,
+) {
+    for (mut transform, player) in &mut characters {
+        let movement_amount = player.speed * time.delta_seconds();
+
+        if input.pressed(KeyCode::W) {
+            transform.translation.y += movement_amount;
+        }
+        if input.pressed(KeyCode::S) {
+            transform.translation.y -= movement_amount;
+        }
+        if input.pressed(KeyCode::D) {
+            transform.translation.x += movement_amount;
+        }
+        if input.pressed(KeyCode::A) {
+            transform.translation.x -= movement_amount;
+        }
+    }
+}
+
